cb72debbd5a4e82421387a9016cc68281fc20dcb
Developer <tyler.duck02@gmail.com>
2018-01-22 20:07:37 -0500

    Added a commented-out collision detection method. Not using it because it is broken.

--- src/hardware/navx.py
+++ src/hardware/navx.py
@@ -11,6 +11,8 @@ from robotpy_ext.common_drivers import navx
 class NavX:
 
     def __init__(self, navx_type):
+        self.prev_x_accel = 0
+        self.prev_y_accel = 0
         if navx_type is robotmap.NavXType.I2C:
             self.navx = navx.AHRS.create_i2c()
         elif navx_type is robotmap.NavXType.SPI:
@@ -27,8 +29,45 @@ class NavX:
         """
         return self.navx.getDisplacementX(), self.navx.getDisplacementY(), self.navx.getDisplacementZ()
 
-        
 
+    def getYaw(self):
+        '''
+
+        Gets the yaw, or z-axis for the robot. Helpful for angle calculations.
+
+        '''
+        return self.navx.getYaw()
+
+    """
+    This is ported code from https://www.pdocs.kauailabs.com/navx-mxp/examples/collision-detection/ to see if RobotPy could implement something like this. 
+
+    Note: This should be a command instead of in here.
+
+    def collision(self, threshold, isEnabled):
+        '''
+
+        Parameters:
+            Threshold, in Gs, at which a hit is considered a collision.
+            isEnabled - A boolean that will show if the robot is enabled.
+        WARNING: NOT STABLE
+
+        '''
+
+        self.collisionDetected = False
+
+        while isEnabled:
+
+            self.current_x_accel = self.navx.getWorldLinearAccelX()
+            self.current_Jerk_x = self.current_x_accel - self.prev_x_accel
+            self.prev_x_accel = self.current_x_accel
+            self.current_y_accel = self.navx.getWorldLinearAccelY()
+            self.current_Jerk_y = self.current_y_accel - self.prev_y_accel
+            self.prev_y_accel = self.current_y_accel
+
+            if abs(self.current_Jerk_x) == threshold and abs(self.current_Jerk_y) == threshold:
+                self.collisionDetected = True
+        return self.collisionDetected
+    """
 
     
 
